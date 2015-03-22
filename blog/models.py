0b447801de81f0b927e3efc0cf6f4acdb3831ee4
Josh Dingus <josh.dingus@gmail.com>
2015-03-22 16:48:01 +0000

    manage add_user added

--- blog/models.py
+++ blog/models.py
@@ -4,6 +4,16 @@ from sqlalchemy import Column, Integer, String, Sequence, Text, DateTime
 
 from database import Base, engine
 
+from flask.ext.login import UserMixin
+
+class User(Base, UserMixin):
+    __tablename__ = "users"
+
+    id = Column(Integer, Sequence("user_id_sequence"), primary_key=True)
+    name = Column(String(128))
+    email = Column(String(128), unique=True)
+    password = Column(String(128))
+
 class Post(Base):
     __tablename__ = "posts"
 
@@ -11,5 +21,5 @@ class Post(Base):
     title = Column(String(1024))
     content = Column(Text)
     datetime = Column(DateTime, default=datetime.datetime.now)
-
+		
 Base.metadata.create_all(engine)
--- manage.py
+++ manage.py
@@ -5,8 +5,29 @@ from blog import app
 
 manager = Manager(app)
 
-from blog.models import Post
+from blog.models import Post, User
 from blog.database import session
+
+from getpass import getpass
+from werkzeug.security import generate_password_hash
+
+@manager.command
+def adduser():
+    name = raw_input("Name: ")
+    email = raw_input("Email: ")
+    if session.query(User).filter_by(email=email).first():
+        print "User with that email address already exists"
+        return
+
+    password = ""
+    password_2 = ""
+    while not (password and password_2) or password != password_2:
+        password = getpass("Password: ")
+        password_2 = getpass("Re-enter password: ")
+    user = User(name=name, email=email,
+                password=generate_password_hash(password))
+    session.add(user)
+    session.commit()
 
 @manager.command
 def seed():
