fd129eeaf9a6bafbe12f4530099ca7f4cde0c08a
Josh Dingus <josh.dingus@gmail.com>
2015-03-23 10:13:47 +0000

    login logout with navbar link added

--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,13 @@
-from flask import render_template, request, redirect, url_for
+from flask import render_template, request, redirect, url_for, flash
 
 from blog import app
 from database import session
-from models import Post
+from models import Post, User
 
 import mistune
-
-@app.route("/modal")
-def modal():
-    return render_template("modal.html")	
+	
+from flask.ext.login import login_user, login_required, logout_user
+from werkzeug.security import check_password_hash
 
 @app.route("/")
 @app.route("/page/<int:page>")
@@ -56,10 +55,12 @@ def edit_post_post(id):
     return redirect(url_for("posts"))	
 	
 @app.route("/post/add", methods=["GET"])
+@login_required
 def add_post_get():
     return render_template("add_post.html")
 	
 @app.route("/post/add", methods=["POST"])
+@login_required
 def add_post_post():
     post = Post(
         title=request.form["title"],
@@ -70,8 +71,31 @@ def add_post_post():
     return redirect(url_for("posts"))
 	
 @app.route("/post/<int:id>/delete", methods=["GET"])
+@login_required
 def delete_post_post(id):
     post = session.query(Post).filter_by(id=id).first()	
     session.delete(post)
     session.commit()
+    return redirect(url_for("posts"))
+
+@app.route("/login", methods=["GET"])
+def login_get():
+    return render_template("login.html")
+
+@app.route("/login", methods=["POST"])
+def login_post():
+    email = request.form["email"]
+    password = request.form["password"]
+    user = session.query(User).filter_by(email=email).first()
+    if not user or not check_password_hash(user.password, password):
+        flash("Incorrect username or password", "danger")
+        return redirect(url_for("login_get"))
+
+    login_user(user)
+    return redirect(request.args.get('next') or url_for("posts"))
+
+@app.route("/logout", methods=["GET"])
+@login_required
+def logout():
+    logout_user()
     return redirect(url_for("posts"))
