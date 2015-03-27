ca4823eb597e9c99ea558a8d6f0357c6037d34b5
Josh Dingus <josh.dingus@gmail.com>
2015-03-27 07:16:41 +0000

    working thru app.run locking issue

--- tests/test_views_0acceptance.py
+++ tests/test_views_0acceptance.py
@@ -14,6 +14,8 @@ from blog import app
 from blog import models
 from blog.database import Base, engine, session
 
+from selenium import webdriver
+
 class TestViews(unittest.TestCase):
     def setUp(self):
         """ Test setup """
@@ -28,25 +30,34 @@ class TestViews(unittest.TestCase):
         session.add(self.user)
         session.commit()
         port = int(os.environ.get('PORT', 8080))
-        self.process = multiprocessing.Process(target=app.run(host='0.0.0.0',port=port))
-# 				target=app.run, kwargs = {'host':'0.0.0.0','port':8080}
+        self.process = multiprocessing.Process(target=app.run(host='localhost',port=8080))
         self.process.start()
         time.sleep(1)
+
 
     def tearDown(self):
         """ Test teardown """
         # Remove the tables and their data from the database
-#         self.process.terminate()
+        self.process.terminate()
         Base.metadata.drop_all(engine)
         self.browser.quit()
 
     def testLoginCorrect(self):
-        self.browser.visit("http://0.0.0.0:8080/login")
-        self.browser.fill("email", "alice@example.com")
-        self.browser.fill("password", "test")
-        button = self.browser.find_by_css("button[type=submit]")
-        button.click()
-        self.assertEqual(self.browser.url, "http://0.0.0.0:8080/")
+			
+        self.driver = webdriver.PhantomJS()
+        self.driver.set_window_size(1120, 550)
+        self.driver.get("https://duckduckgo.com/")
+        self.driver.find_element_by_id('search_form_input_homepage').send_keys("realpython")
+        self.driver.find_element_by_id("search_button_homepage").click()
+        print self.driver.current_url
+        self.driver.quit()
+			
+#         self.browser.visit("http://0.0.0.0:8080/login")
+#         self.browser.fill("email", "alice@example.com")
+#         self.browser.fill("password", "test")
+#         button = self.browser.find_by_css("button[type=submit]")
+#         button.click()
+#         self.assertEqual(self.browser.url, "http://0.0.0.0:8080/")
 
     def testLoginIncorrect(self):
         self.browser.visit("http://0.0.0.0:8080/login")
